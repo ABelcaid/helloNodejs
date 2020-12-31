1b596676e8c457337cfd25608f80300700e88011
belcaid abderrahim <belcaidna@gmail.com>
2020-12-31 02:23:21 +0100

    change hello msg

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// jshint esversion:6
-
 const express = require('express')
-const app = express();
-const port = 80;
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World From Agadir !!');
+  res.send(' Azoul flaoun from Agadir ')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
